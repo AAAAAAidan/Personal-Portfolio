c5a56871287431ce32ebfd163b28aad20e77222d
AAAAAAidan <a.k.zamboni@gmail.com>
2021-10-20 12:10:14 -0700

    Finish dynamic content rendering - Add fetchFolders() method to fetchUtilities - Add dynamic content rendering to Gallery.js - Add randomly rendered content on page load - Fix issue with page div height

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,16 +1,59 @@
 import styles from "../styles/Gallery.module.css"
+import { useEffect, useState } from "react"
 
-export default function Gallery() {
+export default function Gallery(props) {
+
+  const files = props.files
+  const [fileIndex, setFileIndex] = useState(0)
+  const storageUrl = "https://storage.googleapis.com/personal-portfolio-media/"
+
+  useEffect(() => {
+    // Randomize the file index
+    const randomFileIndex = Math.floor(Math.random() * files.length)
+    setFileIndex(randomFileIndex)
+  }, [files])
+
+  // Go to previous file index
+  function previous() {
+    if (fileIndex - 1 < 0) {
+      setFileIndex(files.length - 1)
+    } else {
+      setFileIndex(fileIndex - 1)
+    }
+  }
+
+  // Go to next file index
+  function next() {
+    if (fileIndex + 1 >= files.length) {
+      setFileIndex(0)
+    } else {
+      setFileIndex(fileIndex + 1)
+    }
+  }
+
   return (
     <div>
-      <h2>Folder Description</h2>
-      <div className={styles.gallery}>
-        <button>Previous</button>
-        <img className={styles.image} src="filename" />
-        <button>Next</button>
-      </div>
-      <h3>File Title</h3>
-      <h4>File Description</h4>
+      {files[fileIndex] &&
+        <div>
+          <div className={styles.gallery}>
+            <button onClick={() => previous()}>Previous</button>
+            {files[fileIndex].filename.includes(".mp4") &&
+              <video className={styles.video} src={storageUrl + files[fileIndex].filename} controls>
+                <source type="video/mp4" />
+              </video>
+            }
+            {!files[fileIndex].filename.includes(".mp4") &&
+              <img className={styles.image} src={storageUrl + files[fileIndex].filename} />
+            }
+            <button onClick={() => next()}>Next</button>
+          </div>
+          <h3>{files[fileIndex].title}</h3>
+          <h4>{files[fileIndex].description}</h4>
+        </div>
+      }
+      {!files[fileIndex] &&
+        <p>{fileIndex}</p>
+      }
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,80 @@
 import Gallery from "../components/Gallery.js"
 import { useEffect, useState } from "react"
-import { fetchJson } from "../lib/fetchUtilities.js"
+import { fetchFolders } from "../lib/fetchUtilities.js"
 
 export default function Index() {
 
+  const [bgColor, setBgColor] = useState("rgba(0,0,0,0)")
   const [bgImage, setBgImage] = useState("none")
-  const [bgColor, setBgColor] = useState("rgba(1, 1, 1, 0.3)")
-  const [currentPage, setCurrentPage] = useState(null)
+  const [loadingMessage, setLoadingMessage] = useState("Loading...")
+  const [errorMessage, setErrorMessage] = useState(null)
   const [folders, setFolders] = useState(null)
-
-  const url = "http://localhost:3000"
-
-  fetchJson(url + "/api/folders")
-  .then((response) => {
-    // TODO - Add files to the folders
-    setFolders(response.data)
-  })
+  const [currentFolder, setCurrentFolder] = useState(null)
 
   useEffect(() => {
-    // TODO - Randomize the background image
-    // const fileIndex = Math.floor(Math.random() * folders["Photos"].length)
-    // const photo = folders["Photos"][fileIndex]
-    // const bgUrl = "url(https://storage.googleapis.com/personal-portfolio-media/" + photo.filename + ")"
-    // setBgImage(bgUrl)
+    if (currentFolder) {
+      // Randomize the background image
+      const randomFileIndex = Math.floor(Math.random() * folders[0].files.length)
+      const photo = folders[0].files[randomFileIndex]
+      const bgUrl = "url(https://storage.googleapis.com/personal-portfolio-media/" + photo.filename + ")"
+      setBgImage(bgUrl)
 
-    // Randomize the background color and opacity
-    const red = Math.floor(Math.random() * 256)
-    const green = Math.floor(Math.random() * 256)
-    const blue = Math.floor(Math.random() * 256)
-    const alpha = Math.random()
-    const rgba = "rgba(" + red + "," + green + "," + blue + "," + alpha + ")"
-    setBgColor(rgba)
-  }, [currentPage])
+      // Randomize the background color and opacity
+      const red = Math.floor(Math.random() * 256)
+      const green = Math.floor(Math.random() * 256)
+      const blue = Math.floor(Math.random() * 256)
+      const alpha = Math.random()
+      const rgba = "rgba(" + red + "," + green + "," + blue + "," + alpha + ")"
+      setBgColor(rgba)
+    } else {
+      // Save all folders from the API
+      fetchFolders()
+      .then(([data, error]) => {
+        if (data) {
+          setFolders(data)
+          const randomFolderIndex = Math.floor(Math.random() * data.length)
+          setCurrentFolder(data[randomFolderIndex])
+        } else {
+          setErrorMessage(error)
+        }
+        setLoadingMessage(null)
+      })
+    }
+  }, [currentFolder])
 
   return (
-    <div id="pageDiv" onLoad={() => buildMainContent()} style={{backgroundImage: bgImage}}>
-      <div id="contentDiv" style={{backgroundColor: bgColor}}>
-        <header>
-          <h1><a href="api">Aidan's Personal Portfolio Profile Page</a></h1>
-        </header>
-        <nav>
-          <ul>
-            {folders && folders.map((folder) => (
-              <li key={folder.title}><button onClick={() => setCurrentPage(folder.title)}>{folder.title}</button></li>
-            ))}
-          </ul>
-        </nav>
-        <main>
-          <div id="mainContent">
-            <Gallery />
-          </div>
-        </main>
-        <footer>
-          <h3 id="footerContent">I made this site for fun!</h3>
-        </footer>
-      </div>
+    <div id="pageDiv" style={{backgroundImage: bgImage}}>
+      {loadingMessage &&
+        <h1>{loadingMessage}</h1>
+      }
+      {errorMessage &&
+        <h1>{errorMessage}</h1>
+      }
+      {!loadingMessage && !errorMessage &&
+        <div id="contentDiv" style={{backgroundColor: bgColor}}>
+          <header>
+            <h1><a href="api">Aidan's Personal Portfolio Profile Page</a></h1>
+          </header>
+          <nav>
+            <ul>
+              {folders && folders.map((folder) => (
+                <li key={folder.title}>
+                  <button onClick={() => setCurrentFolder(folder)}>{folder.title}</button>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <main>
+            <div id="mainContent">
+              <h2>{currentFolder.description}</h2>
+              <Gallery files={currentFolder.files} />
+            </div>
+          </main>
+          <footer>
+            <h3 id="footerContent">I made this site for fun!</h3>
+          </footer>
+        </div>
+      }
     </div>
   )
 }
